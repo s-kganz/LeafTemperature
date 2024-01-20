0aeda6755e86ff2bc0ac2c0b2ceefeef9ee1e3fe
s-kganz <ganzkeenan1@gmail.com>
2024-01-19 16:05:11 -0800

    fix mistake in lidar model spec, calculate direct and diffuse coefficients

--- scripts/fit_neon_lidar_k.R
+++ scripts/fit_neon_lidar_k.R
@@ -1,20 +1,26 @@
+# To run the radiation submodel, we need empirical light attenutation
+# coefficients for direct and diffuse radiation. These are calculated empirically
+# based on observations where diffuse and direct light dominate.
+
 library(lidR)
 library(tidyverse)
 
-site_lai <- read_csv("data_out/neon_lai.csv") %>%
-  filter(!(site %in% c("YELL", "SOAP"))) %>%
-  group_by(site) %>%
-  summarize(LAI = median(L, na.rm=TRUE))
+site_lai <- read_csv("data_out/neon_sampled_dhp_lai.csv")
 
+# If there is a prior estimate of LAI in the literature, use it. Otherwise,
+# use the DHP-derived estimate.
 site_info <- read_csv("data_working/neon_site_metadata.csv") %>%
   left_join(site_lai, by=c("site_neon"="site")) %>%
-  drop_na()
+  mutate(
+    lai_best = ifelse(is.na(lai), L_dhp_corrected, lai)
+  )
 
-tc_data <- read_csv("data_out/cross_site_tc_data.csv")
+tc_data <- read_csv("data_out/cross_site_tc_data.csv") %>%
+  mutate(proportion_diffuse = SW_DIF / (SW_IN_TOC + SW_DIF),
+         proportion_diffuse = pmin(pmax(proportion_diffuse, 0), 1))
 
 fit_lidar_constants <- function(site, site_lai, zmax=NA, zmin=1) {
-  # Z where cumulative LAI == total LAI
-  # Measurement height of Io
+  # Z where cumulative LAI == total LAI and measurement height of Io
   if (is.na(zmax)) {
     zmax <- tc_data %>% filter(SITE_NEON == site) %>% 
       pull(z) %>% max() %>% ceiling()
@@ -25,48 +31,49 @@ fit_lidar_constants <- function(site, site_lai, zmax=NA, zmin=1) {
   )) %>%
     filter_poi(Z > zmin, Z < zmax)
   
-  # Calibrate k so that the integral of LAD == LAI. Reverse the order of rows
-  # so we can compute the cumulative LAI looking *down* the canopy.
+  # Calibrate k so that the integral of LAD == LAI. 
   lad_uncal_sum <- LAD(las$Z, k=1, z0 = zmin) %>% pull(lad) %>% sum(na.rm=TRUE)
   k_lai <- lad_uncal_sum / site_lai
   
   lad_profile <- LAD(las$Z, k=k_lai, z0 = zmin)
+  # Reverse the order of rows so we can compute the cumulative LAI looking 
+  # *down* the canopy.
   lad_profile <- lad_profile[nrow(lad_profile):1, ]
   lad_profile$cum_lai <- cumsum(lad_profile$lad)
   
-  # Now calculate transmission profile through the canopy. For each z, calc geometric
-  # mean transmittance to that point in the canopy.
-  tc_site <- tc_data %>% filter(SITE_NEON == site)
+  # Now calculate transmission profile through the canopy under direct and
+  # diffuse conditions. For each z, calc geometric mean transmittance to that 
+  # point in the canopy.
+  tc_site <- tc_data %>% filter(SITE_NEON == site, SW_IN_TOC > 100) %>%
+    select(TIMESTAMP, PPFD_IN_TOC, z, zmax, par_interp, proportion_diffuse)
   
   iv_io_df <- tc_site %>%
-    # Drop night time data
-     filter(SW_IN_TOC > 100) %>%
-    select(TIMESTAMP, PPFD_IN_TOC, z, zmax, par_interp) %>%
-    group_by(TIMESTAMP) %>%
     mutate(iv = par_interp,
-           io = PPFD_IN_TOC) %>%
-    ungroup() %>%
+           io = PPFD_IN_TOC,
+           proportion_diffuse_bin = round(proportion_diffuse, digits=1)) %>%
     mutate(iv_io = iv / io) %>%
-    group_by(z) %>%
+    group_by(z, proportion_diffuse_bin) %>%
     summarize(
-      iv_io_mean = suppressWarnings(log(iv_io)) %>% mean(na.rm=TRUE) %>% exp(),
+      iv_io_mean = suppressWarnings(log(iv_io)) %>% median(na.rm=TRUE) %>% exp(),
       # https://en.wikipedia.org/wiki/Geometric_standard_deviation
       iv_io_sd = suppressWarnings(log(iv_io / iv_io_mean)^2) %>% 
         mean(na.rm=TRUE) %>% sqrt() %>% exp()
     ) %>%
+    drop_na() %>%
     mutate(cum_lai = approx(lad_profile$z, lad_profile$cum_lai, z, rule=2)$y,
            site = site)
   
-  # Force the intercept at 0
-  iv_io_lm <- lm(I(log(iv_io_mean) - 1) ~ cum_lai + 0, data=iv_io_df)
+  # Force the y-intercept at log(iv_io) = 0
+  iv_io_lm <- lm(
+    log(iv_io_mean) ~ cum_lai + cum_lai:proportion_diffuse_bin + 0, 
+    data=iv_io_df
+  )
   
   return(list(
+    site=site,
     data=iv_io_df,
-    constants=list(
-      site = site,
-      k_lai = k_lai,
-      k_rad = -iv_io_lm$coefficients[1]
-    )
+    rad_coef=coef(iv_io_lm),
+    lai_coef=k_lai
   ))
 }
 
@@ -75,7 +82,7 @@ lidar_constants <- lapply(
   function(i) {
     fit_lidar_constants(
       site=site_info$site_neon[i],
-      site_lai=site_info$LAI[i],
+      site_lai=site_info$lai_best[i],
       zmax=site_info$canopy_height[i],
       zmin=1
     )
@@ -83,44 +90,34 @@ lidar_constants <- lapply(
 )
 
 iv_io_data <- lapply(lidar_constants, function(l) l$data) %>% bind_rows()
-lidar_constants_df <- lapply(lidar_constants, function(l) l$constants) %>% bind_rows()
+lidar_constants_df <- lapply(lidar_constants, function(l) {
+  list(
+    site=l$site,
+    lai_coef=l$lai_coef,
+    # Force attenuation coefficients less than 0
+    kb_real=min(unname(l$rad_coef[1]), 0),
+    kd_real=min(unname(l$rad_coef[1] + l$rad_coef[2]), 0)
+  )
+}) %>% bind_rows()
+
+# Extrapolate lines for beam/diffuse coefficients
+clai <- seq(0, 7, length.out=20)
+iv_io_extrapolate <- expand.grid(clai, 1:nrow(lidar_constants_df)) %>%
+  cbind(lidar_constants_df[.$Var2, ]) %>%
+  select(-Var2) %>% rename(clai = Var1) %>%
+  mutate(iv_io_beam = exp(kb_real * clai),
+         iv_io_diff = exp(kd_real * clai)) %>%
+  pivot_longer(contains("iv_io"))
 
 # Plot mean transmission points over the fitted line
-iv_io_lines <- expand.grid(
-    cum_lai=seq(0, 5, length.out=20), 
-    site=lidar_constants_df$site
-  ) %>%
-  mutate(
-    k_rad = lidar_constants_df$k_rad[match(site, lidar_constants_df$site)],
-    iv_io = exp(-k_rad * cum_lai)
-  )
+ggplot(iv_io_data, aes(x=cum_lai, y=iv_io_mean)) +
+  geom_point(aes(color=proportion_diffuse_bin)) +
+  geom_line(data=iv_io_extrapolate, mapping=aes(x=clai, y=value, group=name, linetype=name)) +
+  facet_wrap(~ site, scales="free") +
+  labs(x="Cumulative LAI", y="Proportion available light",
+       color="Proportion diffuse light", name="") +
+  scale_y_log10()
 
-iv_io_data %>%
-  ggplot(aes(x=cum_lai, y=iv_io_mean)) +
-  geom_point() +
-  geom_line(data=iv_io_lines, aes(x=cum_lai, y=iv_io), color="red") +
-  facet_wrap(~ site) +
-  scale_y_log10() +
-  theme_bw()
-
-#write_csv(lidar_constants_df, "data_out/cross_site_lidar_constants.csv")
-
-osbs_ivio <- tc_data %>%
-  filter(SITE_NEON == "OSBS") %>%
-  select(TIMESTAMP, SW_IN_TOC, z, PPFD_IN_TOC, par_interp) %>% 
-  group_by(TIMESTAMP) %>% 
-  mutate(iv = par_interp, io = PPFD_IN_TOC) %>% 
-  ungroup() %>% mutate(iv_io = iv / io) %>%
-  mutate(TIMESTAMP_LOCAL = TIMESTAMP + hours(-5))
-
-osbs_ivio %>%
-  mutate(hour = hour(TIMESTAMP_LOCAL)) %>%
-  ggplot(aes(x=hour, y=SW_IN_TOC)) +
-  geom_boxplot(aes(group=hour))
-
-osbs_ivio %>%
-  ggplot(aes(x=iv_io, y=z)) +
-  geom_boxplot(aes(group=z)) +
-  geom_vline(xintercept=1)
+write_csv(lidar_constants_df, "data_out/neon_lidar_constants.csv")
 
   
